175c24e7ea3bb9d0a1d03223066f9aa2706e9612
Spencer G. Jones <spencer@egovmt.com>
2016-06-24 13:58:27 -0600

    Add actions

--- Program.cs
+++ Program.cs
@@ -7,32 +7,55 @@ using System.Text;
 #if (EF5 || EF6)
 using System.Resources;
 #endif
+using System.Data.Entity.Infrastructure;
 using System.Data.Entity.Migrations.Design;
 
 namespace EfMigrations {
 	public class Program {
 		public static int Main(String[] argsArray) {
 			var args = new Queue<String>(argsArray);
+			String action = null;
 			String baseFolder = null;
+			String binFolder = null;
+			String assemblyName = null;
+			String configPath = null;
 			String migrationName = null;
+			String sourceMigration = null;
+			String targetMigration = null;
+			String connectionName = null;
 			
 			while(args.Count > 0) {
 				var currentArg = args.Peek();
-				if (currentArg == "-n" || currentArg == "--name") {
+				if (currentArg == "-a" || currentArg == "--action") {
+					args.Dequeue();
+					action = args.Dequeue();
+				} else if (currentArg == "-n" || currentArg == "--name") {
 					args.Dequeue();
 					migrationName = args.Dequeue();
-				} else if (currentArg == "-b" || currentArg == "--basefolder") {
+				} else if (currentArg == "--sourceMigration") {
+					args.Dequeue();
+					sourceMigration = args.Dequeue();
+				} else if (currentArg == "--targetMigration") {
+					args.Dequeue();
+					targetMigration = args.Dequeue();
+				} else if (currentArg == "-b" || currentArg == "--baseFolder") {
 					args.Dequeue();
 					baseFolder = args.Dequeue();
+				} else if (currentArg == "--binFolder") {
+					args.Dequeue();
+					binFolder = args.Dequeue();
+				} else if (currentArg == "--assemblyName") {
+					args.Dequeue();
+					assemblyName = args.Dequeue();
+				} else if (currentArg == "--configPath") {
+					args.Dequeue();
+					configPath = args.Dequeue();
+				} else if (currentArg == "--connectionName") {
+					args.Dequeue();
+					connectionName = args.Dequeue();
 				} else {
 					break;
 				}
-			}
-			if (args.Count > 0 && migrationName == null) {
-				migrationName = args.Dequeue();
-			}
-			if (args.Count > 0 && baseFolder == null) {
-				baseFolder = args.Dequeue();
 			}
 			if (baseFolder == null) {
 				baseFolder = Directory.GetCurrentDirectory();
@@ -45,34 +68,76 @@ namespace EfMigrations {
 				}
 			}
 
-			var migrationCreator = new MigrationCreator(baseFolder);
-			migrationCreator.AddMigration(migrationName);
+			var migrationFacade = new MigrationFacade(baseFolder, assemblyName, binFolder, configPath, connectionName);
+			if (action != null) {
+				switch (action) {
+					case "AddMigration":
+						migrationFacade.AddMigration(migrationName);
+						break;
+					case "PendingMigrations":
+						migrationFacade.ListPending();
+						break;
+					case "UpdateDatabase":
+						migrationFacade.UpdateDatabase(targetMigration);
+						break;
+					case "GenerateScript":
+						migrationFacade.GenerateScript(sourceMigration, targetMigration);
+						break;
+					default:
+						throw new Exception("Action: "+action);
+				}
+			}
 			return 0;
 		}
 	}
 
-	public class MigrationCreator {
+	public class MigrationFacade {
 		private const String PROJECT_NAMESPACE = "http://schemas.microsoft.com/developer/msbuild/2003";
 
 		private readonly String _baseFolder;
 		private readonly String _projectPath;
 		private readonly ToolingFacade _fasade;
 
-		public MigrationCreator(String baseFolder) {
+		public MigrationFacade(String baseFolder, String assemblyName, String binFolder, String configPath, String connectionName) {
 			_baseFolder = baseFolder;
-			_projectPath = (new DirectoryInfo(_baseFolder)).GetFiles("*.csproj").First().FullName;
+			if (String.IsNullOrWhiteSpace(binFolder) || String.IsNullOrWhiteSpace(assemblyName)) {
+				_projectPath = (new DirectoryInfo(_baseFolder)).GetFiles("*.csproj").First().FullName;
 
-			var projectXml = XElement.Load(_projectPath);
-			var assemblyName = projectXml.Descendants(XName.Get("AssemblyName", PROJECT_NAMESPACE)).Select(i => i.Value).First();
-			var outputPath = projectXml.Descendants(XName.Get("OutputPath", PROJECT_NAMESPACE)).Select(i => i.Value).First();
-			var binFolder = Path.Combine(_baseFolder, outputPath);
-			var configPath = new DirectoryInfo(_baseFolder).GetFiles("Web.config").Any() ? Path.Combine(_baseFolder, "Web.config") : Path.Combine(_baseFolder, "App.config");
+				var projectXml = XElement.Load(_projectPath);
+				if (String.IsNullOrWhiteSpace(assemblyName)) {
+					assemblyName = projectXml.Descendants(XName.Get("AssemblyName", PROJECT_NAMESPACE)).Select(i => i.Value).First();
+				}
+				if (String.IsNullOrWhiteSpace(binFolder)) {
+					var outputPath = projectXml.Descendants(XName.Get("OutputPath", PROJECT_NAMESPACE)).Select(i => i.Value).First();
+					binFolder = Path.Combine(_baseFolder, outputPath);
+				}
+			}
+			if (String.IsNullOrWhiteSpace(configPath)) {
+				configPath = new DirectoryInfo(_baseFolder).GetFiles("Web.config").Any() ? Path.Combine(_baseFolder, "Web.config") : Path.Combine(_baseFolder, "App.config");
+			}
 
 #if EF6
-			_fasade = new ToolingFacade(assemblyName, assemblyName, null, binFolder, configPath, null, null);
+			_fasade = new ToolingFacade(assemblyName, assemblyName, null, binFolder, configPath, null, String.IsNullOrWhiteSpace(connectionName) ? null : new DbConnectionInfo(connectionName));
 #else
-			_fasade = new ToolingFacade(assemblyName, null, binFolder, configPath, null, null);
+			_fasade = new ToolingFacade(assemblyName, null, binFolder, configPath, null, String.IsNullOrWhiteSpace(connectionName) ? null : new DbConnectionInfo(connectionName));
 #endif
+		}
+
+		public void UpdateDatabase(String targetMigration) {
+			_fasade.Update(targetMigration, false);
+		}
+
+		public void ListPending() {
+			var pending = _fasade.GetPendingMigrations();
+			Console.WriteLine("Pending count: "+pending.Count());
+			foreach(var pend in pending) {
+				Console.WriteLine("\t"+pend);
+			}
+		}
+
+		public void GenerateScript(String sourceMigration, String targetMigration) {
+			var script = _fasade.ScriptUpdate(sourceMigration, targetMigration, false);
+			Console.Write(script);
 		}
 
 		public void AddMigration(String migrationName) {
